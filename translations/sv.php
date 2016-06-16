b998ac1b56b5ec90643ca69fac27092110b9338d
Ola Norlander <ola.norlander@solidar.se>
2016-06-16 10:30:38 +0200

    Added swedish translation.

--- translations/sv.php
+++ translations/sv.php
@@ -1,1 +1,15 @@
 <?php
+
+$timeAgoStrings = array(
+  'aboutOneDay' => "ca en dag sedan",
+  'aboutOneHour' => "ca en timme sedan",
+  'aboutOneMonth' => "ca en månad sedan",
+  'aboutOneYear' => "ca ett år sedan",
+  'days' => "%s dagar sedan",
+  'hours' => "%s timmar sedan",
+  'lessThanAMinute' => "mindre än en minut sedan",
+  'lessThanOneHour' => "%s minuter sedan",
+  'months' => "%s månader sedan",
+  'oneMinute' => "en minut sedan",
+  'years' => "över %s år sedan"
+);
